c62ce562328faf589b2ca4138a8145d47cc48d9b
kubo-hide-kun <hide44041@gmail.com>
2022-08-18 01:51:02 +0900

    tools(website): add script to generate firstview images :wrench:

---
+++ website/scripts/generate-images.sh
@@ -0,0 +1,24 @@
+# USAGE: ルートディレクトリで実行すること
+
+rm ./website/public/assets/first_view_pc/＊
+ffmpeg -i movie.mp4 -r 8 -q:v 1 ./website/public/assets/first_view_pc/%03d.jpg
+
+target_dir="./public/assets/first_view_pc"
+
+function get_last_dir() {
+  path=$1
+
+  array=( `echo $path | tr -s '/' ' '`)
+  last_index=`expr ${#array[@]} - 1`
+  echo ${array[${last_index}]}
+  return 0
+}
+
+for file in ./website/public/assets/first_view_pc/*.jpg
+do
+  jpegoptim -m90 --strip-all "$file"
+
+  file_name=`get_last_dir $file`
+  npm run -w website convert2avif "${target_dir}/${file_name}" -- -d "${target_dir}"
+  npm run -w website convert2webp "${target_dir}/${file_name}" -- -d "${target_dir}"
+done
